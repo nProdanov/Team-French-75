303ee4e9c57425618c86b9825a2391fd20eb8553
Nikolay <nikolayprodanow@gmail.com>
2016-11-05 15:23:53 +0200

    Add mysql reader

---
+++ TravelAgency/TravelAgency.Readers/MySqlReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using TravelAgency.MySqlData;
+
+namespace TravelAgency.Readers
+{
+    public class MySqlReader
+    {
+        private const string ConnectionString = "server=localhost;database=travelagency;uid=root;pwd=1234;";
+
+        public ICollection<TripReportMySqlModel> GetTripReports()
+        {
+            using (var context = new MySqlContext(ConnectionString)) 
+            {
+                return context.TripsReports.ToList();
+            }
+        }
+    }
+}
