c62676f069597d4be550405c83e0889899b5d496
Mariana Naidenova <mariananaidenova@mail.bg>
2016-11-05 16:59:14 +0200

    Add common class.

---
+++ TravelAgency/TravelAgency.Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Common
+{
+    public class Constants
+    {
+        public const string MongoDbConnectionString = "mongodb://localhost";
+        public const string MongoDbDatabaseName = "travelagency";
+        public const string MongoDbCollectionName = "touroperators";
+        public const string MySQLConnectionString = "server=localhost;database=travelagency;uid=root;pwd=1234;";
+        public const string GeneratedReportsPath = "../../../../Generated-Reports/";
+    }
+}
